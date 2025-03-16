1576b533289faaa1d1e8871ddeb58cf8070e35a7
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-03-16 12:38:17 -0700

    coverage

--- test/lender/unit/ExitPositionTest.sol
+++ test/lender/unit/ExitPositionTest.sol
@@ -52,7 +52,7 @@ contract ExitPositionTest is BasicDeploy {
         crossBOracleInstance.setPrice(500e8); // $500 per CROSSB token
 
         // Setup roles
-        vm.prank(guardian);
+        vm.prank(address(timelockInstance));
         ecoInstance.grantRole(REWARDER_ROLE, address(LendefiInstance));
 
         _setupAssets();
