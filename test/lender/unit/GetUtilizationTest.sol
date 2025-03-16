e56718c5453dd7e68ffd381b5ca5cb0717c74611
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-03-16 12:39:13 -0700

    coverage

--- test/lender/unit/GetUtilizationTest.sol
+++ test/lender/unit/GetUtilizationTest.sol
@@ -39,7 +39,7 @@ contract GetUtilizationTest is BasicDeploy {
         stableOracleInstance.setPrice(1e8); // $1 per stable
 
         // Setup roles
-        vm.prank(guardian);
+        vm.prank(address(timelockInstance));
         ecoInstance.grantRole(REWARDER_ROLE, address(LendefiInstance));
 
         _setupAssets();
