d987c82b0f27acd8095112258bb13837aacbbcec
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-03-16 12:39:16 -0700

    coverage

--- test/lender/unit/IsLiquidatableTest.sol
+++ test/lender/unit/IsLiquidatableTest.sol
@@ -38,7 +38,7 @@ contract IsLiquidatableTest is BasicDeploy {
         stableOracleInstance.setPrice(1e8); // $1 per stable
 
         // Setup roles
-        vm.prank(guardian);
+        vm.prank(address(timelockInstance));
         ecoInstance.grantRole(REWARDER_ROLE, address(LendefiInstance));
 
         _setupAssets();
