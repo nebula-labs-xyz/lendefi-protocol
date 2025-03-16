585c37383a8b416c4c5d0dc655b940129607bcb9
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-03-16 12:38:36 -0700

    coverage

--- test/lender/unit/GetLiquidationFeeTest.sol
+++ test/lender/unit/GetLiquidationFeeTest.sol
@@ -48,7 +48,7 @@ contract getPositionLiquidationFeeTest is BasicDeploy {
         stableOracleInstance.setPrice(1e8); // $1 per stable
 
         // Setup roles
-        vm.prank(guardian);
+        vm.prank(address(timelockInstance));
         ecoInstance.grantRole(REWARDER_ROLE, address(LendefiInstance));
 
         _setupAssets();
