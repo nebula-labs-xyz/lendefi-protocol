fb7a95aeaa79adf9e92c75edc6a0275fdcff1bc0
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-03-16 12:39:37 -0700

    coverage

--- test/lender/unit/RepayTest.sol
+++ test/lender/unit/RepayTest.sol
@@ -53,7 +53,7 @@ contract RepayTest is BasicDeploy {
         rwaOracleInstance.setPrice(1000e8); // $1000 per RWA token
 
         // Setup roles
-        vm.prank(guardian);
+        vm.prank(address(timelockInstance));
         ecoInstance.grantRole(REWARDER_ROLE, address(LendefiInstance));
 
         _setupAssets();
